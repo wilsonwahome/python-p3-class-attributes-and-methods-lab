bbda0815bee4ac766360a9f9b8283d859aa0b0b3
wahome <wahomewgitonga@gmail.com>
2024-06-11 19:31:38 +0300

    update

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+
+        # Update genre_count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist_count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+        # Update genres and artists sets
+        Song.genres.add(genre)
+        Song.artists.add(artist)
